1471bec3ca835a6335d88eda401cb4103588dbb6
OSQTR <clecat1005@gmail.com>
2023-05-04 23:45:01 +0900

    framer

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { NavLink } from "react-router-dom";
 
 const NavWrap = styled(motion.div)`
   width: 100%;
@@ -9,22 +10,53 @@ const NavWrap = styled(motion.div)`
 
   position: fixed;
   top: -1px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
 
-const item = {
-  invisible: { opacity: 0, y: 20 },
+const NavItem = styled(NavLink)`
+  margin: 10px;
+  color: black;
+  text-decoration: none;
+  background-color: white;
+  border-radius: 5px;
+  padding: 5px 10px 5px 10px;
+`;
+
+const wrapAni = {
+  list: {
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const itemAni = {
+  hidden: { opacity: 0, y: 20 },
   show: { opacity: 1, y: 0 },
 };
 
-const [id, path, title] = [
+const navItems = [
   [1, "/", "Home"],
   [2, "/Rules", "Rules"],
+  [3, "/My", "My"],
 ];
 
 function Nav() {
   return (
     <>
-      <NavWrap variants={item} initial="invisible" whileInView="show"></NavWrap>
+      <NavWrap variants={wrapAni} animate="list">
+        {navItems.map(([id, path, title]) => (
+          <motion.div variants={itemAni} initial="hidden" whileInView="show">
+            <NavItem id={id} to={path}>
+              {title}
+            </NavItem>
+          </motion.div>
+        ))}
+      </NavWrap>
     </>
   );
 }
